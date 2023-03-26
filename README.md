dca4d892c1145bce82444695a873ba0fd9ca9626
Slarper <naivesimple@outlook.com>
2023-03-26 13:39:56 +0800

    initilize commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# A Custom Widget
+For better batch process
+
+# Installation
+
+Put the .js file into `ComfyUI\web\extensions` folder
---
+++ incremental-widget.js
@@ -0,0 +1,32 @@
+import { app } from "../scripts/app.js";
+
+function indexWidget(node, inputName, inputData) {
+	const index = ComfyWidgets.INT(node, inputName, inputData);
+	const increment = node.addWidget("toggle", "increment after every gen", true, function (v) {}, {
+		on: "enabled",
+		off: "disabled",
+		serialize: false, // Don't include this in prompt.
+	});
+
+	increment.afterQueued = () => {
+		if (increment.value) {
+			index.widget.value += 1;
+		}
+	};
+
+	return { widget: index, randomize: increment };
+}
+
+export const IncrementalWidgets = {
+        "INT:seed": indexWidget,
+}
+
+const ext = {
+	// Unique name for the extension
+	name: "Slarper.IncrementalWidget",
+	async getCustomWidgets(app) {
+                return IncrementalWidgets;
+	}
+};
+
+app.registerExtension(ext);
